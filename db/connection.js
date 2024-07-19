ccbd5ee21c9faaaf002f3a6c57958eaf967240e8
LivEva <guests@hotmail.co.uk>
2024-07-19 16:50:20 +0100

    Adding the repo update from sprint 14

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,28 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+const config = {};
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
+
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+
 }
 
-module.exports = new Pool();
+if (ENV === "production") {
+
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+  
+}
 
 
-//const db = require('../db')
+
+
+
+
+module.exports = new Pool(config);
